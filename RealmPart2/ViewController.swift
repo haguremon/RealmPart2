e4d37015e27e499ecc6a05a744cbe708d8b55771
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-15 17:33:40 +0900

    Initial Commit

---
+++ RealmPart2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPart2
+//
+//  Created by IwasakIYuta on 2021/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
